48b63429a81b91c2e62f734ebf241bab66641b9a
Chatzo <chatzoglakis@gmail.com>
2014-01-29 22:59:00 +0100

    DrawManager first draft

--- Fishy/DrawManager.cpp
+++ Fishy/DrawManager.cpp
@@ -2,6 +2,42 @@
 #include "stdafx.h"
 #include "DrawManager.h"
 
+DrawManager::DrawManager(sf::RenderWindow *p_Window)
+{
+	m_Window = p_Window;
+}
+
+DrawManager::~DrawManager()
+{
+}
+
+void DrawManager::ClearWindow()
+{
+	//Change color to whatever
+	m_Window->clear(sf::Color(0x00,0x00,0x00,0xff));
+	
+}
+
+//Change sf::Sprite to our sprite class, it has to be a sf:Drawable
+void DrawManager::DrawSprite(sf::Sprite * p_pSprite)
+{
+	m_Window->draw(*p_pSprite);
+}
+//Change sf::Sprite to our sprite class, it has to be a sf:Drawable
+void DrawManager::DrawSprite(sf::Sprite * p_pSprite, sf::RenderStates * p_pEffect)
+{
+	m_Window->draw(*p_pSprite, *p_pEffect);
+}
+
+void DrawManager::DisplayWindow()
+{
+	m_Window->display();
+	
+}
+
+
+
+
 
 
 
--- Fishy/DrawManager.h
+++ Fishy/DrawManager.h
@@ -1,4 +1,20 @@
 // DrawManager.h
 #pragma once
 
+class DrawManager
+{
+public:
+	DrawManager(sf::RenderWindow *p_Window);
+	~DrawManager();
 
+
+	void ClearWindow();
+	//Change sf::Sprite to our sprite class, it has to be a sf:Drawable
+	void DrawSprite(sf::Sprite * p_pSprite);
+	//Change sf::Sprite to our sprite class, it has to be a sf:Drawable
+	void DrawSprite(sf::Sprite * p_pSprite, sf::RenderStates * p_pEffect);
+	void DisplayWindow();
+private:
+	sf::RenderWindow * m_Window;
+
+};
