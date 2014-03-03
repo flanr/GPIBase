2ee498941d95e64b61f48f39fbde5c4240b44a5c
flanr <flanr@live.se>
2014-03-03 13:01:50 +0100

    Quick draft of Option Menu, Sprites placed out

--- Fishy/OptionState.cpp
+++ Fishy/OptionState.cpp
@@ -16,6 +16,39 @@ OptionState::OptionState(Core* p_pCore)
 	////////////////
 	m_BackGroundImg = m_SpriteManager->Load("background2.jpg",0,0,1280,720);
 	m_BackGroundImg->setPosition(0,0);
+	// OptionBackground
+	m_OptionBackground = m_SpriteManager->Load("optionsbg.png",0,0,851,518);
+	m_OptionBackground->setPosition(0,0);
+	
+	////////////////
+	//  Buttons   //
+	////////////////
+
+	// Mute Music
+	m_ButtonMusicMute.SetSprite(m_SpriteManager->Load("mute.png",17,15,85,58));
+	m_ButtonMusicMute.SetPosition(m_OptionBackground->getPosition().x + 743,m_OptionBackground->getPosition().y + 205);
+	// Mute Sound  
+	m_ButtonSoundMute.SetSprite(m_SpriteManager->Load("mute.png",17,15,85,58));
+	m_ButtonSoundMute.SetPosition(m_OptionBackground->getPosition().x + 743,m_OptionBackground->getPosition().y + 312);
+	// Fullscreen
+	m_ButtonFullScreen.SetSprite(m_SpriteManager->Load("fullscreen.png",22,27,244,57));
+	m_ButtonFullScreen.SetPosition(m_OptionBackground->getPosition().x + 215,m_OptionBackground->getPosition().y + 420);
+	// Windowed
+	m_ButtonWindowed.SetSprite(m_SpriteManager->Load("windowed.png",22,27,244,57));
+	m_ButtonWindowed.SetPosition(m_OptionBackground->getPosition().x + 485,m_OptionBackground->getPosition().y + 420);
+	// Return
+	m_ButtonReturn.SetSprite(m_SpriteManager->Load("backarrow.png",17,15,85,58));
+	m_ButtonReturn.SetPosition(m_OptionBackground->getPosition().x + 745 ,m_OptionBackground->getPosition().y + 420);
+	
+	/////////////
+	// Sliders //
+	/////////////
+	m_SliderMusicVol.SetSlider(m_OptionBackground->getPosition().x+65,m_OptionBackground->getPosition().y+206,600,25);
+	m_SliderMusicVol.SetColor(sf::Color::White);
+	
+	m_SliderSoundVol.SetSlider(m_OptionBackground->getPosition().x+65,m_OptionBackground->getPosition().y+314,600,25);
+	m_SliderSoundVol.SetColor(sf::Color::White);
+
 
 }
 
@@ -32,6 +65,7 @@ string OptionState::Next()
 
 bool OptionState::EnterState()
 {
+	m_window->setView(m_window->getDefaultView());
 	m_sCurrentState = "OptionState";
 	cout << "OptionState::EnterState" << endl;
 
@@ -71,9 +105,16 @@ void OptionState::HandleInput()
 void OptionState::Draw()
 {
 	m_DrawManager->ClearWindow();
-	
 	m_DrawManager->Draw(m_BackGroundImg);
-	
+	m_DrawManager->Draw(m_OptionBackground);
+	m_DrawManager->Draw(m_ButtonMusicMute.GetSprite());
+	m_DrawManager->Draw(m_ButtonSoundMute.GetSprite());
+	m_DrawManager->Draw(m_ButtonFullScreen.GetSprite());
+	m_DrawManager->Draw(m_ButtonWindowed.GetSprite());
+	m_DrawManager->Draw(m_ButtonReturn.GetSprite());
+	m_DrawManager->DrawSlider(m_SliderMusicVol);
+	m_DrawManager->DrawSlider(m_SliderSoundVol);
+
 	//m_level->Draw(m_DrawManager);
 	m_DrawManager->DisplayWindow();
 }
